06265a62e604af5edf95bee6c0b4a9bf161593c2
leesangha <sanghalee1@naver.com>
2024-01-03 23:20:14 +0900

    card 데모

---
+++ components/etc/CardSection/index.tsx
@@ -0,0 +1,9 @@
+import { Card } from '@/components/ui/Card'
+
+export function CardSection() {
+  return (
+    <div className="flex flex-wrap mx-30 p-30 rounded-[clamp(38px,5vw,100px)] h-[800px] bg-[#354555]">
+      <Card />
+    </div>
+  )
+}
